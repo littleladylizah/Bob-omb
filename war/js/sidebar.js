b0159be7f1cb0528ed778cf82d624759912d3e2e
Tiit Pikma <pikma@hot.ee>
2012-02-27 23:47:22 +0200

    Lisatud algne stiil ning külgriba.

---
+++ war/js/sidebar.js
@@ -0,0 +1,17 @@
+// Script needed to toggle the sidebar
+// Shamelessly stolen from http://code.google.com/edu/submissions/html-css-javascript/
+
+var toggleSidebar = function() {
+  var sidebar = document.getElementById("sidebar-content");
+  if (hasClass(sidebar, "sidebar-off")) {
+    removeClass(sidebar, "sidebar-off");
+  } else {
+    addClass(sidebar, "sidebar-off");
+  }
+};
+
+// Attach listener to toggler
+addEvent(window, "load", function() {
+  var toggler = document.getElementById("sidebar-toggle");
+  addEvent(toggler, "click", toggleSidebar);
+});
