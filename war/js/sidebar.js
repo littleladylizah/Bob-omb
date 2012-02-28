d2e51f372d1fe894d9f7de5264bba79e0ad3fcc8
Tiit Pikma <pikma@hot.ee>
2012-02-28 22:09:57 +0200

    Külgribas hetkel kuvatav link esiletõstetud.

--- war/js/sidebar.js
+++ war/js/sidebar.js
@@ -17,3 +17,32 @@ addEvent(window, "load", function() {
   var toggler = document.getElementById("sidebar-toggle");
   addEvent(toggler, "click", toggleSidebar);
 });
+
+// Script to swap sidebar content
+var showHistory = function(moves) {
+  var movesLink = document.getElementById("moves");
+  var gamesLink = document.getElementById("games");
+
+  if (moves) {
+    removeClass(movesLink, "faded");
+    addClass(gamesLink, "faded");
+  } else {
+    removeClass(gamesLink, "faded");
+    addClass(movesLink, "faded");
+  }
+  // TODO stub
+};
+
+var showMoves = function() {
+  showHistory(true);
+};
+
+var showGames = function() {
+  showHistory(false);
+};
+
+// Attach listeners to links
+addEvent(window, "load", function() {
+  addEvent(document.getElementById("moves"), "click", showMoves);
+  addEvent(document.getElementById("games"), "click", showGames);
+});
