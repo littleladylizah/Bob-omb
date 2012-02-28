886b7f1575cee547214b480f07c7e8f805c6bfd4
Tiit Pikma <pikma@hot.ee>
2012-02-28 17:20:32 +0200

    Mängulaudade lõuendid.

--- war/js/draw.js
+++ war/js/draw.js
@@ -1,13 +1,18 @@
 // Temporary code to test <canvas>
-window.onload = function() {
-  var canvas = document.getElementById("canvas");
+var drawOnCanvas = function(canvas) {
   var ctx = canvas.getContext("2d");
-
   ctx.save();
   ctx.translate(ctx.canvas.width / 2, ctx.canvas.height / 2);
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
   ctx.font = "3em sans-serif";
-  ctx.fillText("This is our canvas", 0, 0);
+  ctx.fillText("This is a canvas", 0, 0);
   ctx.restore();
 };
+
+var drawTest = function() {
+  drawOnCanvas(document.getElementById("player-canvas"));
+  drawOnCanvas(document.getElementById("enemy-canvas"));
+};
+
+addEvent(window, "load", drawTest);
