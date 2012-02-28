98e5be0e6fed7dcb5bb64e8f9bed1a2b660bc162
Tiit Pikma <pikma@hot.ee>
2012-02-28 22:37:55 +0200

    overlay.js vormindus.

--- war/js/overlay.js
+++ war/js/overlay.js
@@ -1,20 +1,13 @@
+// Script to display overlays
+
 $(function() {
+  $("a[rel]").overlay({
+    mask: '#f0f0d8',
+    effect: 'apple',
 
-	// if the function argument is given to overlay,
-	// it is assumed to be the onBeforeLoad event listener
-	$("a[rel]").overlay({
-
-		mask: '#f0f0d8',
-		effect: 'apple',
-
-		onBeforeLoad: function() {
-
-			// grab wrapper element inside content
-			var wrap = this.getOverlay().find(".contentWrap");
-
-			// load the page specified in the trigger
-			wrap.load(this.getTrigger().attr("href"));
-		}
-
-	});
+    onBeforeLoad: function() {
+      var wrap = this.getOverlay().find(".contentWrap");
+      wrap.load(this.getTrigger().attr("href"));
+    }
+  });
 });
