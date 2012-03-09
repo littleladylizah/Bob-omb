cc3d45cc2844c1f2f1fd2e329734c25d6d1dbf55
Tiit Pikma <pikma@hot.ee>
2012-03-09 15:34:44 +0200

    Külgriba JavaScript ümber kirjutatud jQuery peale.

--- war/js/sidebar.js
+++ war/js/sidebar.js
@@ -1,59 +1,46 @@
-// Script needed to toggle the sidebar
-// Shamelessly stolen from http://code.google.com/edu/submissions/html-css-javascript/
-// TODO rewrite using jQuery
+// JavaScript needed for the sidebar
 
-var toggleSidebar = function() {
-  var sidebar = document.getElementById("sidebar-container");
-  if (hasClass(sidebar, "sidebar-off")) {
-    removeClass(sidebar, "sidebar-off");
-    addClass(sidebar, "sidebar-on");
-  } else {
-    removeClass(sidebar, "sidebar-on");
-    addClass(sidebar, "sidebar-off");
-  }
-};
-
-// Attach listener to toggler
-addEvent(window, "load", function() {
-  var toggler = document.getElementById("sidebar-toggle");
-  addEvent(toggler, "click", toggleSidebar);
-});
-
-// Script to swap sidebar content
-var showHistory = function(moves) {
-  var movesLink = document.getElementById("moves");
-  var gamesLink = document.getElementById("games");
-  var content = $("#sidebar-content");
-
-  // TODO use server queries instead of static files
-  if (moves) {
-    removeClass(movesLink, "faded");
-    addClass(gamesLink, "faded");
-    content.load("moves.html");
-  } else {
-    removeClass(gamesLink, "faded");
-    addClass(movesLink, "faded");
-    content.load("games.html");
-  }
-};
-
-var showBoats = function() {
-  $("#moves").addClass("faded");
-  $("#games").addClass("faded");
-  $("#sidebar-content").load("boats.html");
-}
+var movesLink;
+var gamesLink;
+var toggler;
+var container;
+var content;
 
 var showMoves = function() {
-  showHistory(true);
+  movesLink.removeClass("faded");
+  gamesLink.addClass("faded");
+  content.load("moves.html");
 };
 
 var showGames = function() {
-  showHistory(false);
+  gamesLink.removeClass("faded");
+  movesLink.addClass("faded");
+  content.load("games.html");
 };
 
-// Attach listeners to links
-addEvent(window, "load", function() {
-  addEvent(document.getElementById("moves"), "click", showMoves);
-  addEvent(document.getElementById("games"), "click", showGames);
+var showBoats = function() {
+  movesLink.addClass("faded");
+  gamesLink.addClass("faded");
+  content.load("boats.html");
+}
+
+$(window).load(function() {
+  // Select the needed elements
+  movesLink = $("#sidebar-moves");
+  gamesLink = $("#sidebar-games");
+  toggler = $("#sidebar-toggle");
+  container = $("#sidebar-container");
+  content = $("#sidebar-content");
+
+  // Attach listener to toggler
+  toggler.click(function () {
+    container.toggleClass("sidebar-on sidebar-off");
+  });
+
+  // Attach listeners to links
+  movesLink.click(showMoves);
+  gamesLink.click(showGames);
+
+  // Default to showing boats on open
   showBoats();
 });
