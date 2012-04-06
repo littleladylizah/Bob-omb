0d673d0b2b9f819e14ec3402204245803a87f15c
Tiit Pikma <pikma@hot.ee>
2012-04-06 19:09:00 +0300

    Tühjad servletid

---
+++ src/main/java/ee/ut/cs/vl/bobomb/controller/HistoryServlet.java
@@ -0,0 +1,12 @@
+// vim: set sw=4 ts=4 et:
+package ee.ut.cs.vl.bobomb.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/ajax/history")
+public class HistoryServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/ee/ut/cs/vl/bobomb/controller/LeaderboardServlet.java
@@ -0,0 +1,12 @@
+// vim: set sw=4 ts=4 et:
+package ee.ut.cs.vl.bobomb.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/ajax/leaderboard")
+public class LeaderboardServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+}
