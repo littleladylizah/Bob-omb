53f6c51d2b061236bd5fad0f8f8a282a905d425c
Tiit Pikma <pikma@hot.ee>
2012-04-11 05:21:35 +0300

    Overlayde sisu uuenema

--- src/main/webapp/js/ui/overlay.js
+++ src/main/webapp/js/ui/overlay.js
@@ -1,13 +1,29 @@
 // Script to display overlays
 
 $(function() {
+  var timer = null;
+
+  var poll = function(url) {
+    $("#overlay #content-wrap").load(url);
+    timer = setTimeout(function() {
+      poll(url);
+    }, 2000);
+  };
+
+  var stopPolling = function() {
+    clearTimeout(timer);
+  };
+
   $("a[rel]").overlay({
     mask: '#f0f0d8',
     effect: 'apple',
 
     onBeforeLoad: function() {
-      var wrap = this.getOverlay().find("#content-wrap");
-      wrap.load(this.getTrigger().attr("href"));
+      poll(this.getTrigger().attr("href"));
+    },
+
+    onBeforeClose: function() {
+      clearTimeout(timer);
     }
   });
 });
