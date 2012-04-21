f48668d19e8473ffae0a364b91452ad955f86d3e
Tiit Pikma <pikma@hot.ee>
2012-04-21 18:41:49 +0300

    Tugimeetodid replay jaoks

---
+++ src/main/webapp/js/replay.js
@@ -0,0 +1,98 @@
+// ---------------------------------------
+// Make sure we have Web Storage support
+// ---------------------------------------
+
+if typeof(localStorage) == "undefined" {
+  alert("Puudub HTML5 Web Storage tugi!");
+};
+
+// ------------------------------
+// Basic accessors and mutators
+// ------------------------------
+
+var storeGet = function(key) {
+  return JSON.parse(localStorage[key]);
+};
+
+var storePut = function(key, value) {
+  localStorage[key] = JSON.stringify(value);
+};
+
+// ------------------------------------------
+// Storing and retrieving games
+// ------------------------------------------
+
+var GAMES_KEY = "games";
+
+var getGames = function() {
+  return storeGet(GAMES_KEY);
+};
+
+var setGames = function(games) {
+  storePut(GAMES_KEY, games);
+};
+
+var storeGame = function(game) {
+  var games = getGames();
+  games.push(game);
+  setGames(games);
+};
+
+var getGame = function(index) {
+  return getGames()[index];
+};
+
+var countGames = function() {
+  return getGames().length;
+};
+
+// ------------------
+// Replaying a game
+// ------------------
+
+var replayMoves;
+var replayIndex;
+var replayID = -1;
+
+var replayGame = function(game) {
+  stopReplay();
+  resetCanvases();
+  replayMoves = game;
+  replayIndex = 0;
+  startReplay();
+};
+
+var playNext = function() {
+  if (replayIndex >= replayMoves.length) {
+    stopReplay();
+    return;
+  }
+  // TODO
+  replayIndex++;
+};
+
+var playPrev = function() {
+  if (replayIndex <= 0) {
+    return;
+  }
+  // TODO
+  replayIndex--;
+};
+
+var startReplay = function() {
+  replayID = setInterval(playNext, 1000);
+};
+
+var stopReplay = function() {
+  clearInterval(replayID);
+};
+
+// ----------------------------------------
+// Make sure we have the GAMES_KEY object
+// ----------------------------------------
+
+$(window).load(function() {
+  if (!storeGet(GAMES_KEY)) {
+    storePut(GAMES_KEY, []);
+  }
+});
